d2944b30eb6aab5a573582e9d77fd693927a7520
M Waleed <mwfarrukh@gmail.com>
2022-09-04 16:41:51 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  SafeAreaView,
+  Animated,
+} from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+const images = [
+  require("./assets/lisa.jpg"),
+  require("./assets/ana.jpg"),
+  require("./assets/bale.jpg"),
+];
+
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const ANCHO_CONTENEDOR = width * 0.7;
+const ESPACIO_CONTENEDOR = (width - ANCHO_CONTENEDOR) / 2;
+const ESPACIO = 10;
+const ALTURA_BACKDROP = height * 0.5;
+
+function Backdrop({ scrollX }) {
+  return (
+    <View
+      style={[
+        {
+          position: "absolute",
+          height: ALTURA_BACKDROP,
+          top: 0,
+          width: width,
+        },
+        StyleSheet.absoluteFillObject,
+      ]}
+    >
+      {images.map((imagen, index) => {
+        const inputRange = [
+          (index - 1) * ANCHO_CONTENEDOR,
+          index * ANCHO_CONTENEDOR,
+          (index + 1) * ANCHO_CONTENEDOR,
+        ];
+
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0, 1, 0],
+        });
+        return (
+          <Animated.Image
+            key={index}
+            source={imagen}
+            style={[
+              { width: width, height: ALTURA_BACKDROP, opacity },
+              StyleSheet.absoluteFillObject,
+            ]}
+          />
+        );
+      })}
+      <LinearGradient
+        colors={["transparent", "white"]}
+        style={{
+          width,
+          height: ALTURA_BACKDROP,
+          position: "absolute",
+          bottom: 0,
+        }}
+      />
+    </View>
+  );
+}
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <Backdrop scrollX={scrollX} />
+      <Animated.FlatList
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        snapToAlignment="start"
+        contentContainerStyle={{
+          paddingTop: 200,
+          paddingHorizontal: ESPACIO_CONTENEDOR,
+        }}
+        snapToInterval={ANCHO_CONTENEDOR}
+        decelerationRate={0}
+        scrollEventThrottle={16}
+        data={images}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * ANCHO_CONTENEDOR,
+            index * ANCHO_CONTENEDOR,
+            (index + 1) * ANCHO_CONTENEDOR,
+          ];
+
+          const scrollY = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, -50, 0],
+          });
+          return (
+            <View style={{ width: ANCHO_CONTENEDOR }}>
+              <Animated.View
+                style={{
+                  marginHorizontal: ESPACIO,
+                  padding: ESPACIO,
+                  borderRadius: 34,
+                  // backgroundColor: "#fff",
+                  alignItems: "center",
+                  transform: [{ translateY: scrollY }],
+                }}
+              >
+                <Image source={item} style={styles.posterImage} />
+                
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  posterImage: {
+    width: "100%",
+    height: ANCHO_CONTENEDOR * 1.2,
+    resizeMode: "cover",
+    borderRadius: 15,
+    margin: 0,
+    marginBottom: 10,
   },
 });
